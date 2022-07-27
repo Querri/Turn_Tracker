a3a607563e566a824491d0c9475235e51d7bc1d1
Veera Kansikas <veera.kansikas@tuni.fi>
2022-07-27 19:07:46 +0300

    Trying different colors

--- turn_tracker/lib/main.dart
+++ turn_tracker/lib/main.dart
@@ -77,19 +77,40 @@ const Color brown_medium = Color(0xFF211A15);
 const Color brown_light = Color(0xFF875D36);
 const Color brown_bright = Color(0xFF754C24);
 
+const Color orange_light = Color(0xFFffccb3);
+const Color orange_dark = Color(0xFFffb366);
+const Color salmon_light = Color(0xFFff5050);
+const Color salmon_dark = Color(0xFFff3333);
+
+const Color night_light = Color(0xFF364156);
+const Color night_medium = Color(0xFF212D40);
+const Color night_dark = Color(0xFF11151C);
+const Color red_light = Color(0xFFEE5C42);
+const Color red_bright = Color(0xFFD90429);
+
+
+const Color green_white = Color(0xFFCAD2C5);
+const Color green_light = Color(0xFF84A98C);
+const Color green_medium = Color(0xFF52796F);
+const Color green_dark = Color(0xFF354F52);
+const Color green_black = Color(0xFF2F3E46);
+
+
+const Color idk_dark = Color(0xFF1E2531);
+
 const ColorScheme _colorScheme = ColorScheme(
   brightness: Brightness.dark,
 
-  primary: brown_light,
-  primaryVariant: brown_medium,
-  onPrimary: black,
+  primary: idk_dark,
+  primaryVariant: idk_dark,
+  onPrimary: white,
 
-  secondary: brown_bright,
-  secondaryVariant: brown_medium,
-  onSecondary: black,
+  secondary: red_light,
+  secondaryVariant: red_light,
+  onSecondary: white,
 
-  background: brown_dark,
-  onBackground: brown_light,
+  background: black,
+  onBackground: white,
 
   error: black,
   onError: white,
