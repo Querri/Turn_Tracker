4f60b9bf8028df7bcd1c9dc8d1afac5d25234982
Veera Kansikas <veera.kansikas@tuni.fi>
2022-07-27 22:19:42 +0300

    Remove rotation from the ready animation

--- turn_tracker/lib/pages/game_view_animator.dart
+++ turn_tracker/lib/pages/game_view_animator.dart
@@ -14,7 +14,6 @@ class AnimatedReady extends StatefulWidget {
 
 class _AnimatedReadyState extends State<AnimatedReady> with TickerProviderStateMixin {
   AnimationController _controller;
-  AnimationController _repeatController;
 
   @override
   void initState() {
@@ -24,34 +23,34 @@ class _AnimatedReadyState extends State<AnimatedReady> with TickerProviderStateM
         duration: const Duration(milliseconds: 2000),
         vsync: this
     );
-
-    _repeatController = AnimationController(
-        duration: const Duration(seconds: 15),
-        vsync: this
-    );
   }
 
   @override
   Widget build(BuildContext context) {
 
     if (widget.isReady) {
-      _repeatController.repeat();
       if (widget.shouldAnimateReady) {
         _controller.reset();
         _controller.forward().orCancel;
       }
     } else {
-      _repeatController.stop();
-      if (widget.shouldAnimateReady) {
-        _controller.reset();
-        _controller.forward().orCancel;
-      }
+      //TODO check when toggle
+      _controller.reset();
+      _controller.reverse().orCancel;
     }
+
+    /*
+    return Center(
+      child: StaggerAnimation2(
+        controller: _controller.view,
+        buttonSize: widget.buttonSize,
+      ),
+    );
+    */
 
     return Center(
       child: StaggerAnimation(
         controller: _controller.view,
-        repeatController: _repeatController.view,
         buttonSize: widget.buttonSize,
       ),
     );
@@ -60,78 +59,96 @@ class _AnimatedReadyState extends State<AnimatedReady> with TickerProviderStateM
 
 
 /// Custom animation sequence for ready button click.
-///
-/// The button shrinks for a moment and makes one fast rotation.
-/// After that it continues repeating a slow linear rotation.
 class StaggerAnimation extends StatelessWidget {
-  StaggerAnimation({ Key key, this.controller, this.repeatController, this.buttonSize }) :
+  StaggerAnimation({ Key key, this.controller, this.buttonSize }) :
 
-        // Sequence: size of the button shrinks and grows back.
+  // Sequence: size of the button shrinks and grows back.
         size = TweenSequence(
           <TweenSequenceItem<double>>[
             TweenSequenceItem<double>(
-              tween: Tween(begin: buttonSize, end: buttonSize*0.9)
+              tween: Tween(begin: buttonSize, end: buttonSize*0.8)
                   .chain(CurveTween(curve: Curves.easeOutQuart)),
               weight: 30.0,
             ),
             TweenSequenceItem<double>(
-              tween: Tween(begin: buttonSize*0.9, end: buttonSize)
+              tween: Tween(begin: buttonSize*0.8, end: buttonSize*1.4)
                   .chain(CurveTween(curve: Curves.easeInOutBack)),
               weight: 70.0,
             ),
           ],
-        ).animate(
-            (CurvedAnimation(parent: controller, curve: Interval(0.0, 0.5)))),
-
-        // Fast bouncy spin for button click.
-        rotation = Tween<double>(begin: 0.0, end: 1.0,).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.0, 1.00,
-              curve: Curves.easeOutCirc,
-            ),
-          ),
-        ),
-
-        // Slow linear spin for button in ready state.
-        repeatRotation = Tween<double>(begin: 0.0, end: 1.0,).animate(
-          CurvedAnimation(
-            parent: repeatController,
-            curve: Curves.linear,
-          ),
-        ),
+        ).animate((CurvedAnimation(parent: controller, curve: Interval(0.0, 0.3)))),
 
         super(key: key);
 
   final AnimationController controller;
-  final AnimationController repeatController;
   final Animation<double> size;
   final double buttonSize;
-  final Animation<double> rotation;
-  final Animation<double> repeatRotation;
 
   @override
   Widget build(BuildContext context) {
-    return RotationTransition(
-      turns: rotation,
-      child: RotationTransition(
-        turns: repeatRotation,
-        child: Container(
-          alignment: Alignment.center,
-          child: AnimatedBuilder(
-            animation: controller,
-            builder: (context, child) {
-              return Image(
-                width: size.value,
-                height: size.value,
-                image: AssetImage('1024_ready_inactive.png'),
-              );
-            }
-          ),
-        ),
+    return Container(
+      alignment: Alignment.center,
+      child: AnimatedBuilder(
+          animation: controller,
+          builder: (context, child) {
+            return Image(
+              width: size.value,
+              height: size.value,
+              image: AssetImage('1024_ready_inactive.png'),
+            );
+          }
       ),
     );
   }
 }
 
+
+
+/*
+/// Custom animation sequence for ready button click.
+///
+/// The button shrinks for a moment and makes one fast rotation.
+/// After that it continues repeating a slow linear rotation.
+class StaggerAnimation2 extends StatelessWidget {
+  StaggerAnimation2({ Key key, this.controller, this.buttonSize }) :
+
+      // Sequence: size of the button shrinks and grows back.
+      size = TweenSequence(
+        <TweenSequenceItem<double>>[
+          TweenSequenceItem<double>(
+            tween: Tween(begin: buttonSize, end: buttonSize*0.8)
+                .chain(CurveTween(curve: Curves.easeOutQuart)),
+            weight: 30.0,
+          ),
+          TweenSequenceItem<double>(
+            tween: Tween(begin: buttonSize*0.8, end: buttonSize*1.4)
+                .chain(CurveTween(curve: Curves.easeInOutBack)),
+            weight: 70.0,
+          ),
+        ],
+      ).animate((CurvedAnimation(parent: controller, curve: Interval(0.0, 0.3)))),
+
+        super(key: key);
+
+  final AnimationController controller;
+  final Animation<double> size;
+  final double buttonSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: AnimatedBuilder(
+        animation: controller,
+        builder: (context, child) {
+          return Image(
+            width: size.value,
+            height: size.value,
+            image: AssetImage('1024_ready_inactive.png'),
+          );
+        }
+      ),
+    );
+  }
+}
+ */
